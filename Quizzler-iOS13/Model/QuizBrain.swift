478c255a5dc1de76dba0ea7e46b182f06b3ace66
Faiq Ali <faiqueali017@gmail.com>
2020-12-02 17:44:35 +0500

    Model of application updated according to the multiple choice questions

--- Quizzler-iOS13/Model/QuizBrain.swift
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct QuizBrain {
-    
+    /** Normal True/ False Questions
     let quiz = [
         Question(q: "A slug's blood is green.", a: "True"),
         Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
@@ -23,6 +23,20 @@ struct QuizBrain {
         Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
         Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
         Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+    ]*/
+    
+    let quiz = [
+        Question(q: "Which is the largest organ in the human body?", a: ["Heart", "Skin", "Large Intestine"], correctAnswer: "Skin"),
+        Question(q: "Five dollars is worth how many nickels?", a: ["25", "50", "100"], correctAnswer: "100"),
+        Question(q: "What do the letters in the GMT time zone stand for?", a: ["Global Meridian Time", "Greenwich Mean Time", "General Median Time"], correctAnswer: "Greenwich Mean Time"),
+        Question(q: "What is the French word for 'hat'?", a: ["Chapeau", "Écharpe", "Bonnet"], correctAnswer: "Chapeau"),
+        Question(q: "In past times, what would a gentleman keep in his fob pocket?", a: ["Notebook", "Handkerchief", "Watch"], correctAnswer: "Watch"),
+        Question(q: "How would one say goodbye in Spanish?", a: ["Au Revoir", "Adiós", "Salir"], correctAnswer: "Adiós"),
+        Question(q: "Which of these colours is NOT featured in the logo for Google?", a: ["Green", "Orange", "Blue"], correctAnswer: "Orange"),
+        Question(q: "What alcoholic drink is made from molasses?", a: ["Rum", "Whisky", "Gin"], correctAnswer: "Rum"),
+        Question(q: "What type of animal was Harambe?", a: ["Panda", "Gorilla", "Crocodile"], correctAnswer: "Gorilla"),
+        Question(q: "Where is Tasmania located?", a: ["Indonesia", "Australia", "Scotland"], correctAnswer: "Australia")
+        
     ]
     
     var questionNumber = 0
@@ -30,24 +44,26 @@ struct QuizBrain {
     
     
     //MARK:- Helper Methods
-    mutating func checkAnswer(answer userAnswer: String) -> Bool {
-        if userAnswer == quiz[questionNumber].answer {
-            //User got it right
-            score += 1
-            return true
-        } else {
-            //User got it wrong
-            return false
-        }
-    }
     
+    //To display the question
     func getQuestionText() -> String {
         return quiz[questionNumber].text
     }
     
+    //To display the progress
     func getProgress() -> Float{
         let progress = Float(questionNumber)/Float(quiz.count)
         return progress
+    }
+    
+    //To get the ans
+    func getAnswers() -> [String] {
+        return quiz[questionNumber].answers
+    }
+    
+    //returns user score
+    func getScore() -> Int {
+        return score
     }
     
     /**Swift struct are immutable (cannot be changed). To safe change an object you have to tell it by assigning the word 'mutating' before the func */
@@ -62,9 +78,15 @@ struct QuizBrain {
         }
     }
     
-    //returns user score
-    func getScore() -> Int {
-        return score
+    mutating func checkAnswer(answer userAnswer: String) -> Bool {
+        if userAnswer == quiz[questionNumber].rightAnswer {
+            //User got it right
+            score += 1
+            return true
+        } else {
+            //User got it wrong
+            return false
+        }
     }
     
 }
